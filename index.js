633a96bf42aa90fe103ae86bcbf178a1107837eb
Benjamin Pang <benjinpang@gmail.com>
2017-05-10 13:43:24 -0700

    add more functions, change structure

--- index.js
+++ index.js
@@ -5,27 +5,29 @@ sl=require('./stdlib.js')
 fs=require('fs')
 
 stack=[]
+ln=[0]
 input=process.argv[process.argv.length-1]
 
 exec=x=>{
-  parser.parse(x).map(a=>
+  parser.parse(x).map(a=>a&&(
     a.big&&a[1]&&a[0]=='\\'?
       stack.unshift(a.slice(1))
     :a.big&&a[1]&&a[0]=='#'?
       0
     :a.big&&sl[a]?
-      sl[a]()
+      sl[a](x)
     :stack.unshift(a)
-  )
+  ))
 }
+lne=x=>(exec(lines[ln[0]]),ln.shift())
 
 require.main!=module&&(module.exports=this)
 ~process.argv.indexOf('-h')?
   console.log(fs.readFileSync('help.txt')+'')
 :input?
-  (exec((lines=(
+  (lines=(
     ~process.argv.indexOf('-e')?
       input
     :fs.readFileSync(input)+''
-  ).split`\n`)[0]),~process.argv.indexOf('-o')&&console.log(stack.join`\n`))
+  ).split`\n`,lne(),~process.argv.indexOf('-o')&&console.log(stack.join`\n`))
 :console.log("ERR: no input argument specified")
--- stdlib.js
+++ stdlib.js
@@ -1,4 +1,6 @@
 module.exports={
+  "id":x=>(id=stack.shift(),stack.unshift(lines.find(a=>(a.match`^ *#([^\d. ])`||[])[1]==id).replace(/^ *#[^\d. ]/,''))),
+
   "_":x=>stack.unshift(-stack.shift()),
   "+":x=>stack.unshift(stack.shift()+stack.shift()),
   "-":x=>stack.unshift(stack.shift()-stack.shift()),
@@ -7,11 +9,17 @@ module.exports={
   "%":x=>stack.unshift(stack.shift()%stack.shift()),
   "^":x=>stack.unshift(Math.pow(stack.shift(),stack.shift())),
 
-  "dup":x=>stack.unshift(stack[stack.length-1]),
+  "dup":x=>stack.unshift(stack[0]),
   "drop":x=>stack.shift(),
   "swap":x=>stack.splice(1,0,stack.shift()),
   "rot":x=>stack.unshift(stack.splice(2,1)),
   "rot_":x=>stack.splice(2,0,stack.shift()),
+  "nip":x=>stack.splice(1,1),
+  "tuck":x=>stack.splice(2,0,stack[0]),
+  "over":x=>stack.unshift(stack[1]),
 
-  "el":x=>exec(lines[stack.pop()]),
+  "el":x=>(ln.unshift(stack.shift()),lne()),
+  "en":x=>(ln.unshift(ln[0]+1),lne()),
+  "ep":x=>(ln.unshift(ln[0]-1),lne()),
+  "es":x=>exec(stack.shift()),
 0:0}
