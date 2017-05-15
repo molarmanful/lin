4ff308cd76e61f910ade9fcfc2c77cf76ecf5ece
Benjamin Pang <benjinpang@gmail.com>
2017-05-14 18:30:19 -0700

    add more stdlib functions

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 #!/bin/env node
 
+//modules
 parser=require('./lin-peg.js')
 sl=require('./stdlib.js')
 fs=require('fs')
+unesc=require('unescape-js')
 
+//variables
 stack=[]
 ln=[0]
 ids={}
 input=process.argv[process.argv.length-1]
 
-get=x=>stack[(x%stack.length+stack.length)%stack.length]
+//convenience functions for stdlib
+mod=(x,y)=>(x%y+y)%y
+form=x=>stack.map(a=>JSON.stringify(a)).reverse().join`\n`
+get=x=>stack[mod(x,stack.length)]
 splice=(x,y=1,z)=>z==[]._?
-  stack.splice((x%stack.length+stack.length)%stack.length,y)
-:stack.splice((x%stack.length+stack.length)%stack.length,y,z)
-shift=x=>stack.shift()
+  stack.splice(mod(x,stack.length),y)
+:stack.splice(mod(x,stack.length),y,z)
+shift=x=>stack.shift()||0
 unshift=x=>stack.unshift(x)
 
+//exec wrapper for line jumping
+lne=x=>(exec(lines[ln[0]]),ln.shift())
+
+//main exec function
 exec=x=>{
-  parser.parse(x).map(a=>
+  x&&x.replace(/\s/g,'')&&parser.parse(x).map(a=>
+    //refs
     a.big&&a[1]&&a[0]=='\\'?
       unshift(a.slice(1))
+    //ids
     :a.big&&a[1]&&a[0]=='#'?
       0
+    //matched functions
     :a.big&&sl[a]?
       sl[a](x)
+    //everything else (numbers, unmatched functions)
     :unshift(a)
   )
 }
-lne=x=>(exec(lines[ln[0]]),ln.shift())
 
+//export as module
 require.main!=module&&(module.exports=this)
+//help flag
 ~process.argv.indexOf('-h')?
   console.log(fs.readFileSync('help.txt')+'')
+//read argument
 :input?
   (lines=(
     ~process.argv.indexOf('-e')?
       input
     :fs.readFileSync(input)+''
-  ).split`\n`,lne(),~process.argv.indexOf('-o')&&console.log(stack))
+  ).split`\n`,lne())
 :console.log("ERR: no input argument specified")
--- stdlib.js
+++ stdlib.js
@@ -2,20 +2,30 @@ module.exports=$={}
 $["gi"]=x=>(//
     unshift(
       ids[x=shift()]||(
-        line=lines.find(a=>(a.match`^ *#([^\d. ])`||[])[1]==id),
-        line&&(ids[id]=line.replace(/^ *#[^\d. ]/,''))
+        line=lines.find(a=>(a.match`^ *#([^\d. ])`||[])[1]==x),
+        line&&(ids[x]=unesc(line.replace(/^ *#[^\d. ]/,'')))
       )
     )
   )
+$["gs"]=x=>unshift(form())
 $["si"]=x=>ids[shift()]=shift()
+
+$["out"]=x=>process.stdout.write(''+shift())
 
 $["_"]=x=>unshift(-shift())
 $["+"]=x=>unshift(shift()- -shift())
-$["-"]=x=>unshift(shift()-shift())
+$["-"]=x=>($.swap(),unshift(shift()-shift()))
 $["*"]=x=>unshift(shift()*shift())
-$["/"]=x=>unshift(shift()/shift())
-$["%"]=x=>unshift(shift()%shift())
-$["^"]=x=>unshift(Math.pow(shift(),shift()))
+$["/"]=x=>($.swap(),unshift(shift()/shift()))
+$["//"]=x=>($.swap(),unshift(Math.floor(shift()/shift())))
+$["%"]=x=>($.swap(),unshift(mod(shift(),shift())))
+$["/%"]=x=>($.over(),$.over(),$['//'](),$.rot_(),$['%']())
+$["^"]=x=>($.swap(),unshift(Math.pow(shift(),shift())))
+
+$["~"]=x=>unshift(~shift())
+$["&"]=x=>($.swap(),unshift(shift()&shift()))
+$["|"]=x=>($.swap(),unshift(shift()|shift()))
+$["$"]=x=>($.swap(),unshift(shift()^shift()))
 
 $["pick"]=x=>unshift(get(shift()))
 $["nix"]=x=>splice(shift())
@@ -23,24 +33,20 @@ $["roll"]=x=>(x=get(0),$.pick(),unshift(x+1),$.nix())
 $["roll_"]=x=>splice(shift(),0,shift())
 $["dup"]=x=>unshift(stack[0])
 $["drop"]=x=>shift()
-$["rot"]=x=>unshift(splice(2,1))
+$["rot"]=x=>unshift(splice(2,1)[0])
 $["rot_"]=x=>($.rot(),$.rot())
 $["swap"]=x=>($.dup(),$.rot_(),$.drop())
 $["nip"]=x=>($.swap(),$.drop())
-$["tuck"]=x=>($.dup(),$.rot())
+$["tuck"]=x=>($.dup(),$.rot_())
 $["over"]=x=>($.swap(),$.tuck())
 $["clr"]=x=>stack=[]
 $["rev"]=x=>stack.reverse()
+$["len"]=x=>unshift(stack.length)
 
 $["el"]=x=>(ln.unshift(shift()),lne())
 $["en"]=x=>(ln[0].big||ln.unshift(ln[0]+1),lne())
 $["ep"]=x=>(ln[0].big||ln.unshift(ln[0]-1),lne())
-$["ei"]=x=>(ln.unshift(''),id=shift(),
-    exec(
-      ids[x=shift()]||(
-        line=lines.find(a=>(a.match`^ *#([^\d. ])`||[])[1]==id),
-        line&&(ids[id]=line.replace(/^ *#[^\d. ]/,''))
-      )
-    )
-  )
+$["ei"]=x=>($.gi(),$.es())
 $["es"]=x=>(ln.unshift(''),exec(shift()))
+$["e&"]=x=>($.swap(),shift()?$.ei():shift())
+$["e|"]=x=>($.swap(),shift()?shift():$.ei())
