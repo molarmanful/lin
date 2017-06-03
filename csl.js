530f943877dcaa936887bff3a186eed1461c8fc6
Benjamin Pang <benjinpang@gmail.com>
2017-06-03 09:51:31 -0700

    oops

--- csl.js
+++ csl.js
@@ -7,6 +7,6 @@ fs.writeFileSync('stdlib.js',
 )
 fs.writeFileSync('commands.md',
   `# Commands\n**NOTE:** Anything with "index [number]" refers to the item at that specific index on the stack. "index 0" refers to the top of the stack, "index 1" refers to the second-from-top of stack, etc.\n\n${
-    (fs.readFileSync('stdlib')+'').match(/^(\S+) .+\/\/(.+)$/gm).join`\n`.replace(/^(\S+) .+\/\/(.+)$/gm,'- <code>$1</code>: $2')
+    (fs.readFileSync('stdlib')+'').match(/^(\S+) .+ \/\/(.+)$/gm).join`\n`.replace(/^(\S+) .+\/\/(.+)$/gm,'- <code>$1</code>: $2')
   }`
 )
