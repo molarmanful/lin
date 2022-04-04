3f0c3c64e6e690b1186e9c038ad39710cb5f9bc1
Benjamin Pang <molarmanful@users.noreply.github.com>
2022-04-04 15:24:35 +0400

    fix doc compiler

--- comp/csl.js
+++ comp/csl.js
@@ -2,9 +2,10 @@
 
 let fs = require('fs')
 let sl = fs.readFileSync('./src/stdlib.js') + ''
+let exp = /\/\/ (.+)\r*\n+SL\["(.+)"\]/g
 
 fs.writeFileSync('./docs/commands.md',
   `# Commands\n**NOTE:** Anything with "index [number]" refers to the item at that specific index on the stack. "index 0" refers to the top of the stack, "index 1" refers to the second-from-top of stack, etc.\n\n${
-    sl.match(/\/\/ (.+)\nSL\["(.+)"\]/g).join`\n`.replace(/\/\/ (.+)\nSL\["(.+)"\]/g, '- <code>$2</code>: $1')
+    sl.match(exp).join`\n`.replace(exp, '- <code>$2</code>: $1')
   }`
 )
