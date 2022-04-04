a1df669a085a6a843edf72186f7ca280aa80d653
Benjamin Pang <molarmanful@users.noreply.github.com>
2022-04-04 17:03:47 +0400

    revert doc compiler changes due to crlf issue

--- comp/csl.js
+++ comp/csl.js
@@ -2,7 +2,7 @@
 
 let fs = require('fs')
 let sl = fs.readFileSync('./src/stdlib.js') + ''
-let exp = /\/\/ (.+)\r*\n+SL\["(.+)"\]/g
+let exp = /\/\/ (.+)\n+SL\["(.+)"\]/g
 
 fs.writeFileSync('./docs/commands.md',
   `# Commands\n**NOTE:** Anything with "index [number]" refers to the item at that specific index on the stack. "index 0" refers to the top of the stack, "index 1" refers to the second-from-top of stack, etc.\n\n${
