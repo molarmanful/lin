0934cd66de1a8382ea30a5ff8152b2c305c3711b
Benjamin Pang <molarmanful@users.noreply.github.com>
2022-06-29 12:59:29 +0400

    oops

--- src/bridge.js
+++ src/bridge.js
@@ -2,7 +2,6 @@ export * as fs from 'fs'
 export * as cp from 'child_process'
 export * as _ from 'lodash'
 export * as rls from 'readline-sync'
-export * as iter from 'iter-tools-es'
 
 export {default as parse} from './parser.js'
 export {default as unesc} from './unesc.js'
